fd9b5fd33c03c6cc76615fc428e62e9934b9585e
Samuel Glew <samuel.glew@gmail.com>
2021-09-19 17:27:23 +0930

    predefining error

--- new_panel/server.js
+++ new_panel/server.js
@@ -219,6 +219,7 @@ async function runCMD(req, res, action) {
                     detached: true
                 });
                 sp.on('error', (err) => {
+                    let error = {};
                     error["repsonse"] = err.message;
 
                     res.send({
