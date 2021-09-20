5718319739c69aaa20c171c079ca1ace27b7475e
samglew <sglew@jamesanthonyconsulting.com.au>
2021-09-20 10:20:23 +0930

    fixing bad copy+paste

--- new_panel/public/js/app.js
+++ new_panel/public/js/app.js
@@ -221,14 +221,14 @@ window.addEventListener('load', () => {
                 case "Online" : {
                     if(!$(".Primary.Server .status-section .cog.icon").hasClass("green")){
                         $(".Primary.Server .status-section .cog.icon").removeClass("red").addClass("green")
-                        $(".Primary.Server .status-section .status-text").css("color","green").text(statusPrimary
+                        $(".Primary.Server .status-section .status-text").css("color","green").text(statusPrimary)
                     }
                 }; break;
                 
                 case "Offline" : {
                     if(!$(".Primary.Server .status-section .cog.icon").hasClass("red")){
                         $(".Primary.Server .status-section .cog.icon").addClass("red").removeClass("green")
-                        $(".Primary.Server .status-section .status-text").css("color","red").text(statusPrimary
+                        $(".Primary.Server .status-section .status-text").css("color","red").text(statusPrimary)
                     }
                 }; break;
                 
@@ -266,21 +266,21 @@ window.addEventListener('load', () => {
                 case "Online" : {
                     if(!$(".Secondary.Server .status-section .cog.icon").hasClass("green")){
                         $(".Secondary.Server .status-section .cog.icon").removeClass("red").addClass("green")
-                        $(".Secondary.Server .status-section .status-text").css("color","green").text(statusSecondary
+                        $(".Secondary.Server .status-section .status-text").css("color","green").text(statusSecondary)
                     }
                 }; break;
                 
                 case "Offline" : {
                     if(!$(".Secondary.Server .status-section .cog.icon").hasClass("red")){
                         $(".Secondary.Server .status-section .cog.icon").addClass("red").removeClass("green")
-                        $(".Secondary.Server .status-section .status-text").css("color","red").text(statusSecondary
+                        $(".Secondary.Server .status-section .status-text").css("color","red").text(statusSecondary)
                     }
                 }; break;
                 
                 case "Starting..." : {
                     if($(".Secondary.Server .status-section .cog.icon").hasClass("green") || $(".Secondary.Server .status-section .cog.icon").hasClass("red")){
                         $(".Secondary.Server .status-section .cog.icon").removeClass("red").removeClass("green")
-                        $(".Secondary.Server .status-section .status-text").css("color","").text(statusSecondary
+                        $(".Secondary.Server .status-section .status-text").css("color","").text(statusSecondary)
                     }
                 }; break;
             }
